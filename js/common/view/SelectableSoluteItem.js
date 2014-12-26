0282f22bd99a307759f9a6ed5062814c0fec61cf
ashraf <ashraf.abuthahir@gmail.com>
2014-12-26 14:54:48 +0530

    SelectableSoluteItem ported

--- js/common/view/SelectableSoluteItem.js
+++ js/common/view/SelectableSoluteItem.js
@@ -1,19 +1,30 @@
-//// Copyright 2002-2011, University of Colorado
-//package edu.colorado.phet.sugarandsaltsolutions.common.view;
-//
-//import edu.colorado.phet.sugarandsaltsolutions.common.model.DispenserType;
-//
-///**
-// * Item to be displayed in a DispenserRadioButtonSet
-// *
-// * @author Sam Reid
-// */
-//public class SelectableSoluteItem {
-//    public final String name;
-//    public final DispenserType dispenserType;
-//
-//    public SelectableSoluteItem( String name, DispenserType dispenserType ) {
-//        this.name = name;
-//        this.dispenserType = dispenserType;
-//    }
-//}
+//  Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ * Item to be displayed in a DispenserRadioButtonSet
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Sharfudeen Ashraf (For Ghent University)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+
+  /**
+   *
+   * @param {string} name
+   * @param {DispenserType}dispenserType
+   * @constructor
+   */
+  function SelectableSoluteItem( name, dispenserType ) {
+    this.name = name;
+    this.dispenserType = dispenserType;
+  }
+
+  return inherit( Object, SelectableSoluteItem );
+
+} );
+
+
