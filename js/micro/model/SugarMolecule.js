bc5082c36ce6abf2201f7dd9eafab31088e104b3
ashraf <ashraf.abuthahir@gmail.com>
2015-01-11 11:41:05 +0530

    Sugar Molecule Added

--- js/micro/model/SugarMolecule.js
+++ js/micro/model/SugarMolecule.js
@@ -1,3 +1,46 @@
+// Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ * A single sugar molecule (such as glucose or sucrose), which is used to build up sugar crystals
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Sharfudeen Ashraf (For Ghent University)
+ */
+define( function( require ) {
+  'use strict';
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Compound = require( 'SUGAR_AND_SALT_SOLUTIONS/common/model/Compound' );
+  var Constituent = require( 'SUGAR_AND_SALT_SOLUTIONS/common/model/Constituent' );
+  var Vector2 = require( 'DOT/Vector2' );
+  var SugarAndSaltSharedProperties = require( 'SUGAR_AND_SALT_SOLUTIONS/common/SugarAndSaltSharedProperties' );
+
+  /**
+   *
+   * @param {Vector2} relativePosition
+   * @param {number} angle
+   * @param {ProjectedPositions} positions //Positions for the atoms within the molecule
+   * @constructor
+   */
+  function SugarMolecule( relativePosition, angle, positions ) {
+    var self = this;
+    Compound.call( this, relativePosition, angle );
+    //Add the glucose molecule atoms in the right locations, and in the right z-ordering
+    _.each( positions.getAtoms(), function( atomPosition ) {
+      self.constituents.add( new Constituent( atomPosition.createConstituent(),
+        relativePosition.plus( atomPosition.position.times( SugarAndSaltSharedProperties.sizeScale.get() ) ) ) );
+
+    } );
+
+    //Update positions so the lattice position overwrites constituent particle positions
+    this.stepInTime( Vector2.ZERO, 0.0 );
+  }
+
+  return inherit( Compound, SugarMolecule );
+
+} );
+
+
 //// Copyright 2002-2012, University of Colorado
 //package edu.colorado.phet.sugarandsaltsolutions.micro.model;
 //
@@ -24,12 +67,6 @@
 //                          ProjectedPositions positions ) {
 //        super( relativePosition, angle );
 //
-//        //Add the glucose molecule atoms in the right locations, and in the right z-ordering
-//        for ( AtomPosition atomPosition : positions.getAtoms() ) {
-//            constituents.add( new Constituent<SphericalParticle>( atomPosition.createConstituent(), relativePosition.plus( atomPosition.position.times( sizeScale.get() ) ) ) );
-//        }
-//
-//        //Update positions so the lattice position overwrites constituent particle positions
-//        stepInTime( ZERO, 0.0 );
+
 //    }
 //}
