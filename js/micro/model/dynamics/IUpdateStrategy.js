b75c77b8f5b35c6fdf6b0dbd6a9ea4d3ad45d4f7
samreid <reids@colorado.edu>
2015-11-06 21:07:15 -0700

    updated copyright info using grunt update-copyright-dates (2nd pass), see  https://github.com/phetsims/chipper/issues/403

--- js/micro/model/dynamics/IUpdateStrategy.js
+++ js/micro/model/dynamics/IUpdateStrategy.js
@@ -1,4 +1,4 @@
-// Copyright 2014, University of Colorado Boulder
+// Copyright 2014-2015, University of Colorado Boulder
 
 /**
  * Strategy pattern interface for updating particles as time passes, see UpdateStrategy.
