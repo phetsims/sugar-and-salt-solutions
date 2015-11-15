5c81ab7c98283ae2904296dc27d0896cc57a7b03
samreid <reids@colorado.edu>
2015-11-14 22:48:27 -0700

    Fixed a problem that was causing crash on startup

--- js/common/SugarAndSaltSharedProperties.js
+++ js/common/SugarAndSaltSharedProperties.js
@@ -1,1 +1,16 @@
-// Copyright 2014-2015, University of Colorado Boulder
+// Copyright 2015, University of Colorado Boulder
+
+/**
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var Property = require( 'AXON/Property' );
+
+  return {
+    sizeScale: new Property( 1 )
+  };
+} );
